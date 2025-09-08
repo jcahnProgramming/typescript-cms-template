ac2a5fc7e4715e61da68b2d9ade49fcbed9fff75
jcahnProgramming <jamie.cahn93@gmail.com>
2025-09-08 01:13:40 -0600

    Fixed issue - server now runs.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-images: { remotePatterns: [{ protocol: 'https', hostname: '**.supabase.co' }] },
-experimental: { reactCompiler: true },
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: '**.supabase.co' },
+      { protocol: 'https', hostname: '**.supabase.in' }
+    ]
+  }
 };
 export default nextConfig;
